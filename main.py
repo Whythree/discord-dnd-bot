3aea20f55ca6b4eab61817989320d65b5d114c7a
Whythree <whythree1.dollars@gmail.com>
2026-02-13 23:29:58 +0100

    adding simple static api call

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import os
 from dotenv import load_dotenv
-
 import discord
 from discord.ext import commands
-
+import aiohttp 
+import asyncio
 
 load_dotenv()
 api_key = str(os.getenv("API_KEY"))
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot =commands.Bot(command_prefix="!", intents=intents)
 
+
 @bot.event
 async def on_ready():
+    #Dynamically adding a new session attribute to bot object
+    bot.session = aiohttp.ClientSession(base_url="https://www.dnd5eapi.co/api/2014/")
     print(f"Bot logged on as {bot.user}")
+
+@bot.event
+async def on_close():
+    await bot.session.close()
 
 @bot.command()
 async def ping(context):
     await context.send("Pong!")
 
+@bot.command()
+async def spell(context, *, spellname):
+    print(spellname)
+    async with bot.session.get("spells/fireball") as response:
+        print(response.status)
+        print(await response.json())
 
 bot.run(api_key)
