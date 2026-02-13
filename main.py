aacaf8c82de3bbbd9f369e9829beb073708e0a3b
Whythree <whythree1.dollars@gmail.com>
2026-02-13 22:04:06 +0100

    Adding minimal example

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import discord
 import os
+from dotenv import load_dotenv
 
-#ToDo: Eigene Implementation des Beispiels schreiben.
-intents = 
-client = discord.Client()
+import discord
+from discord.ext import commands
 
-@client.event
+
+load_dotenv()
+api_key = str(os.getenv("API_KEY"))
+
+bot =commands.Bot(command_prefix="!", intents=discord.Intents.default())
+
+@bot.event
 async def on_ready():
-    print("We have logged in as {0.user}".format(client))
+    print(f"Bot logged on as {bot.user}")
+
+@bot.command()
+async def ping(context):
+    await context.send("Pong!")
 
 
+bot.run(api_key)
