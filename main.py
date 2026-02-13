daa13bdf704cee749fea8f982a97e5d5453bc9c8
Whythree <whythree1.dollars@gmail.com>
2026-02-13 22:17:46 +0100

    Working minimal example

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from discord.ext import commands
 load_dotenv()
 api_key = str(os.getenv("API_KEY"))
 
-bot =commands.Bot(command_prefix="!", intents=discord.Intents.default())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot =commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
